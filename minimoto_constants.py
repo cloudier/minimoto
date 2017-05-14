80382dd9ffa1b1af4e080a0fc63bcc060d5d8e2d
C <cldtu@yahoo.com.au>
2017-05-14 14:22:28 +0000

    setup adds info to pickle file for shutdown

--- minimoto_constants.py
+++ minimoto_constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-GROUP_DOMAIN = "group12.2017.cs9243.unsw.edu.au"
-INVERSE_GROUP_DOMAIN = "au.edu.unsw.cs9243.2017.group12"
+GROUP_DOMAIN = "test1.group12.2017.cs9243.unsw.edu.au"
+INVERSE_GROUP_DOMAIN = "au.edu.unsw.cs9243.2017.group12.test1"
 USER = "ubuntu"
 TRANSCODING_REQUESTS_QUEUE_NAME = "group12-transcoding-requests-queue"
 INPUT_BUCKET_NAME = INVERSE_GROUP_DOMAIN + ".input"
