2d2457c69a8d0dd6afb09821a3f5896972611458
C <cldtu@yahoo.com.au>
2017-05-16 17:10:09 +0000

    watchdog status done

--- minimoto_constants.py
+++ minimoto_constants.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
-GROUP_DOMAIN = "group12.2017.cs9243.unsw.edu.au"
-INVERSE_GROUP_DOMAIN = "au.edu.unsw.cs9243.2017.group12"
+GROUP_DOMAIN = "test.group12.2017.cs9243.unsw.edu.au"
+INVERSE_GROUP_DOMAIN = "au.edu.unsw.cs9243.2017.group12.test"
 USER = "ubuntu"
 TRANSCODING_REQUESTS_QUEUE_NAME = "group12-transcoding-requests-queue"
 INPUT_BUCKET_NAME = INVERSE_GROUP_DOMAIN + ".input"
 OUTPUT_BUCKET_NAME = INVERSE_GROUP_DOMAIN + ".output"
 SERVICE_AMI_NAME = INVERSE_GROUP_DOMAIN + ".service.ami"
+INSTANCE_TAG_NAME = GROUP_DOMAIN + ".tag"
+INSTANCE_TAG_VALUE = GROUP_DOMAIN + ".value"
 SECURITY_GROUP_NAME = "group12-ssh-security-group"
 IAM_ROLE_NAME = "group12-iam-role"
 IAM_PROFILE_NAME = "group12-iam-profile"
