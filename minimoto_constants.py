a21017b5705da73c3477d8294f40a5c9a4f7e67e
chybby <achybby@gmail.com>
2017-05-22 08:08:37 +1000

    cleanup for submit

--- minimoto_constants.py
+++ minimoto_constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-GROUP_DOMAIN = "test.group12.2017.cs9243.unsw.edu.au"
-INVERSE_GROUP_DOMAIN = "au.edu.unsw.cs9243.2017.group12.test"
+GROUP_DOMAIN = "group12.2017.cs9243.unsw.edu.au"
+INVERSE_GROUP_DOMAIN = "au.edu.unsw.cs9243.2017.group12"
 USER = "ubuntu"
 TRANSCODING_REQUESTS_QUEUE_NAME = "group12-transcoding-requests-queue"
 INPUT_BUCKET_NAME = INVERSE_GROUP_DOMAIN + ".input"
@@ -14,4 +14,3 @@ IAM_ROLE_NAME = "group12-iam-role"
 IAM_PROFILE_NAME = "group12-iam-profile"
 IAM_TRUST_POLICY = "iam_trust_policy.json"
 MINIMOTO_PICKLE_FILE = "minimoto_info.pickle"
-
